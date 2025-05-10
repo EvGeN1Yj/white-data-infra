ebfef82577d4d4abb1a8784207f3aa7c5d2c7871
EvGeN1Yj <propastinevgenij@gmail.com>
2025-05-10 08:57:30 +0300

    build lab2

--- lab1_service/main.py
+++ lab1_service/main.py
@@ -1,3 +1,156 @@
+'''
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+from typing import List, Optional
+import psycopg2
+import logging
+from datetime import date
+from elasticsearch import Elasticsearch
+from neo4j import GraphDatabase
+import redis
+import json
+
+# Настройка логгера
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+app = FastAPI()
+
+# Модели данных
+class User(BaseModel):
+    username: str
+
+
+# Настройки аутентификации
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    # В реальном приложении здесь должна быть проверка токена
+    # Для примера просто возвращаем тестового пользователя
+    return User(username="testuser")
+
+es = Elasticsearch('http://localhost:9200')
+
+# Postgres
+pg_conn = psycopg2.connect(host="localhost", port="5432", database="university_db", user="user",
+                           password="password")
+
+# Redis
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# NEO4j
+neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+
+
+
+@app.get("/reports/low_attendance/", response_model=List)
+async def get_low_attendance_report(
+    search_term: str,
+    start_date: str,
+    end_date: str,
+    current_user: User = Depends(get_current_user)
+):
+
+    try:
+        # Подключение к Elasticsearch
+
+        query = {
+            "query": {
+                "match": {
+                    "description": search_term
+                }
+            }
+        }
+        # Выполнение запроса
+        response = es.search(index="lecture_materials",body=query)
+        # Сбор id из результатов
+        ids = [int(hit["_id"]) for hit in response["hits"]["hits"]]
+        #######################################################################
+        query = """
+            MATCH (gr:Group)-[h:HAS_SCHEDULE]->(lec:Lecture)
+            WHERE date(h.attendance_date) >= date($start_date) 
+              AND date(h.attendance_date) <= date($end_date)
+            RETURN lec.id
+            ORDER BY h.attendance_date
+            """
+
+        with neo4j_driver.session() as session:
+            result = session.run(query, start_date=start_date, end_date=end_date)
+            neo_ids=[int(x[0]) for x in result]
+        #########################################################################
+        common_elements = [value for value in ids if value in neo_ids]
+        logging.INFO('common els ',common_elements)
+        if len(common_elements)<1:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+            return 'no lections'
+        # SQL-запрос для получения данных
+        query = f"""
+            SELECT 
+                l.topic,
+                s.id AS student_id,
+                COUNT(CASE WHEN a.status = 'presence' THEN 1 END) * 100.0 / COUNT(a.id) AS percents
+            FROM 
+                lectures l
+            JOIN 
+                schedule sch ON l.id = sch.lecture_id
+            JOIN 
+                attendance a ON sch.id = a.schedule_id
+            JOIN 
+                students s ON a.student_id = s.id
+            WHERE 
+                l.id IN ({ ''.join(map(str, common_elements))})
+            GROUP BY 
+                l.topic, s.id
+            ORDER BY 
+                percents ASC
+            LIMIT 10;
+            """
+
+        # Создание курсора и выполнение запроса
+        cursor = pg_conn.cursor()
+        cursor.execute(query)
+        # Получение всех строк результата
+        rows = cursor.fetchall()
+        cursor.close()
+
+        # Формирование ответа в формате JSON
+        response = []
+        for row in rows:
+            topic = row[0]
+            student_id = row[1]
+            percents = row[2]
+
+            # Получение информации о студенте из Redis
+            student_info = redis_client.get(
+                f'student:{student_id}')  # Предполагается, что данные хранятся по ключу 'student:{id}'
+
+            # Если информация о студенте найдена, добавляем её в ответ
+            if student_info:
+                student_info = json.loads(student_info)  # Преобразуем строку JSON в словарь
+                response.append({
+                    'topic': topic,
+                    'student_id': student_id,
+                    'percents': percents,
+                    'student_info': student_info,
+                    'start_date': start_date,
+                    'end_date': end_date,
+                    'termin':search_term
+                })
+
+        return response
+
+
+    except Exception as e:
+        logger.error(f"Error: {str(e)}")
+        raise HTTPException(status_code=500, detail="Service error")
+'''
+
+
+'''
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
@@ -34,16 +187,165 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 
 # Подключение к Elasticsearch
-es = Elasticsearch('http://elasticsearch:9200')
+es = Elasticsearch('http://localhost:9200')
 
 #Postgres
-pg_conn = psycopg2.connect(host="postgres", port="5432", database="university_db", user="user", password="password")
+pg_conn = psycopg2.connect(host="localhost", port="5432", database="university_db", user="user", password="password")
 
 # Redis
-redis_client = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 # NEO4j
-neo4j_driver = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "password"))
+neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+
+@app.get("/reports/low_attendance/", response_model=List)
+async def get_low_attendance_report(
+    search_term: str,
+    start_date: str,
+    end_date: str,
+    current_user: User = Depends(get_current_user)
+):
+
+    #try:
+    query = {
+        "query": {
+            "match": {
+                "description": search_term
+            }
+        }
+    }
+    # Выполнение запроса
+    response = es.search(index="lecture_materials",body=query)
+    # Сбор id из результатов
+    ids = [int(hit["_id"]) for hit in response["hits"]["hits"]]
+    #######################################################################
+    query = """
+        MATCH (gr:Group)-[h:HAS_SCHEDULE]->(lec:Lecture)
+        WHERE date(h.attendance_date) >= date($start_date) 
+          AND date(h.attendance_date) <= date($end_date)
+        RETURN lec.id
+        ORDER BY h.attendance_date
+        """
+
+    with neo4j_driver.session() as session:
+        result = session.run(query, start_date=start_date, end_date=end_date)
+        neo_ids=[int(x[0]) for x in result]
+    #########################################################################
+    if len(neo_ids) == 0 or len(ids)==0:
+        return 'no lections'
+    common_elements = [value for value in ids if value in neo_ids]
+    if len(common_elements)<1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+        return 'no lections'
+    # SQL-запрос для получения данных
+    query = f"""
+        SELECT 
+            l.topic,
+            s.id AS student_id,
+            COUNT(CASE WHEN a.status = 'presence' THEN 1 END) * 100.0 / COUNT(a.id) AS percents
+        FROM 
+            lectures l
+        JOIN 
+            schedule sch ON l.id = sch.lecture_id
+        JOIN 
+            attendance a ON sch.id = a.schedule_id
+        JOIN 
+            students s ON a.student_id = s.id
+        WHERE 
+            l.id IN ({ ''.join(map(str, common_elements))})
+        GROUP BY 
+            l.topic, s.id
+        ORDER BY 
+            percents ASC
+        LIMIT 10;
+        """
+
+    # Создание курсора и выполнение запроса
+    cursor = pg_conn.cursor()
+    cursor.execute(query)
+
+    # Получение всех строк результата
+    rows = cursor.fetchall()
+
+    # Формирование ответа в формате JSON
+    response = []
+    for row in rows:
+        topic = row[0]
+        student_id = row[1]
+        percents = row[2]
+
+        # Получение информации о студенте из Redis
+        student_info = redis_client.get(
+            f'student:{student_id}')  # Предполагается, что данные хранятся по ключу 'student:{id}'
+
+        # Если информация о студенте найдена, добавляем её в ответ
+        if student_info:
+            student_info = json.loads(student_info)  # Преобразуем строку JSON в словарь
+            response.append({
+                'topic': topic,
+                'student_id': student_id,
+                'percents': percents,
+                'student_info': student_info,
+                'start_date': start_date,
+                'end_date': end_date,
+                'termin':search_term
+            })
+
+    return response
+
+    
+    except Exception as e:
+        logger.error(f"Error: {str(e)}, {e}")
+        raise HTTPException(status_code=500, detail="Service error")
+'''
+
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+from typing import List, Optional
+import psycopg2
+import logging
+from datetime import date
+from elasticsearch import Elasticsearch
+from neo4j import GraphDatabase
+import redis
+import json
+
+# Настройка логгера
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+app = FastAPI()
+
+# Модели данных
+class User(BaseModel):
+    username: str
+
+
+# Настройки аутентификации
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    # В реальном приложении здесь должна быть проверка токена
+    # Для примера просто возвращаем тестового пользователя
+    return User(username="testuser")
+
+
+# Подключение к Elasticsearch
+es = Elasticsearch('http://localhost:9200')
+
+#Postgres
+pg_conn = psycopg2.connect(host="localhost", port="5432", database="university_db", user="user", password="password")
+
+# Redis
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# NEO4j
+neo4j_driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
 
 @app.get("/reports/low_attendance/", response_model=List)
 async def get_low_attendance_report(
@@ -142,5 +444,5 @@ async def get_low_attendance_report(
     except Exception as e:
         logger.error(f"Error: {str(e)}")
         raise HTTPException(status_code=500, detail="Service error")
-    finally:
-        cursor.close()
+    # finally:
+    #     cursor.close()
--- lab2_service/main.py
+++ lab2_service/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+import datetime
+import logging
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from typing import List, Optional
 import psycopg2
-from psycopg2 import sql
-import logging
+from neo4j import GraphDatabase
 
 # Настройка логгера
 logger = logging.getLogger(__name__)
@@ -13,221 +13,112 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
+
+# ----- Configuration -----
+PG_HOST = "postgres"
+PG_PORT = 5432
+PG_DB = "university_db"
+PG_USER = "user"
+PG_PASS = "password"
+
+NEO4J_URI = "bolt://neo4j:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "password"
+
+# Init FastAPI
 app = FastAPI()
 
+# Init Postgres sync connection
+pg_conn = psycopg2.connect(
+    host=PG_HOST,
+    port=PG_PORT,
+    database=PG_DB,
+    user=PG_USER,
+    password=PG_PASS
+)
+pg_conn.autocommit = True
 
-# Модели данных
-class User(BaseModel):
-    username: str
+# Init Neo4j sync driver
+neo4j_driver = GraphDatabase.driver(
+    NEO4J_URI,
+    auth=(NEO4J_USER, NEO4J_PASSWORD)
+)
 
-
-class ClassroomRequirement(BaseModel):
+# ----- Pydantic models -----
+class LectureReport(BaseModel):
+    course_id: int
     course_name: str
-    lecture_name: str
-    required_capacity: int
-    tech_requirements: List[str]
-    total_students: int
-    semester: int
+    lecture_id: int
+    lecture_topic: str
+    tech_requirements: Optional[str]
+    attendees_count: int
+
+class RoomReportResponse(BaseModel):
     year: int
+    semester: int
+    data: List[LectureReport]
 
 
-# Настройки аутентификации
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+class LectureInfo(BaseModel):
+    course_name: str
+    topic: str
+    tech_requirements: str
+    total_listeners: int
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    # Упрощенная проверка токена для тестирования
-    # В реальном приложении нужно проверять JWT токен
-    return User(username="testuser")
-
-
-# Подключение к БД
-def get_db_connection():
-    return psycopg2.connect(
-        dbname="university_db",
-        user="user",
-        password="password",
-        host="postgres"
-    )
-
-
-@app.get("/reports/classroom_requirements/", response_model=List[ClassroomRequirement])
-async def get_classroom_requirements(
-        semester: int,
-        year: int,
-        current_user: User = Depends(get_current_user)
-):
-    """
-    Возвращает отчет о требованиях к аудиториям для заданного семестра и года.
-    Включает информацию о курсах, лекциях, количестве студентов и технических требованиях.
-    """
-    conn = get_db_connection()
-    cursor = conn.cursor()
+@app.get("/auditorium-requirements", response_model=[])
+def get_auditorium_requirements(year: int = Query(...), semester: int = Query(...)):
+    result = []
 
     try:
-        query = """
-                SELECT c.name                           AS course_name, \
-                       l.name                           AS lecture_name, \
-                       COUNT(DISTINCT s.student_number) AS total_students, \
-                       l.tech_equipment, \
-                       g.formation_year AS year,
-                CASE 
-                    WHEN EXTRACT(MONTH FROM sch.timestamp) BETWEEN 9 AND 12 THEN 1
-                    WHEN EXTRACT(MONTH FROM sch.timestamp) BETWEEN 1 AND 6 THEN 2
-                    ELSE 0
-                END \
-                AS semester
-            FROM 
-                lecture l
-                JOIN course c ON l.id_course = c.id
-                JOIN schedule sch ON l.id = sch.id_lecture
-                JOIN groups g ON sch.id_group = g.id
-                JOIN student s ON g.id = s.id_group
-            WHERE 
-                (EXTRACT(MONTH FROM sch.timestamp) BETWEEN 9 AND 12 AND \
-                %s \
-                = \
-                1 \
-                AND \
-                EXTRACT \
-                ( \
-                YEAR \
-                FROM \
-                sch \
-                . \
-                timestamp \
-                ) \
-                = \
-                %s \
-                )
-                OR
-                ( \
-                EXTRACT \
-                ( \
-                MONTH \
-                FROM \
-                sch \
-                . \
-                timestamp \
-                ) \
-                BETWEEN \
-                1 \
-                AND \
-                6 \
-                AND \
-                %s \
-                = \
-                2 \
-                AND \
-                EXTRACT \
-                ( \
-                YEAR \
-                FROM \
-                sch \
-                . \
-                timestamp \
-                ) \
-                = \
-                %s \
-                )
-                GROUP \
-                BY
-                c \
-                . \
-                name, \
-                l \
-                . \
-                name, \
-                l \
-                . \
-                tech_equipment, \
-                g \
-                . \
-                formation_year, \
-                semester
-                HAVING
-                CASE
-                WHEN \
-                EXTRACT \
-                ( \
-                MONTH \
-                FROM \
-                sch \
-                . \
-                timestamp \
-                ) \
-                BETWEEN \
-                9 \
-                AND \
-                12 \
-                THEN \
-                1
-                WHEN \
-                EXTRACT \
-                ( \
-                MONTH \
-                FROM \
-                sch \
-                . \
-                timestamp \
-                ) \
-                BETWEEN \
-                1 \
-                AND \
-                6 \
-                THEN \
-                2
-                ELSE \
-                0
-                END \
-                = \
-                %s
-                AND \
-                EXTRACT \
-                ( \
-                YEAR \
-                FROM \
-                sch \
-                . \
-                timestamp \
-                ) \
-                = \
-                %s \
-                """
+        cur = pg_conn.cursor()
 
-        cursor.execute(query, (semester, year, semester, year, semester, year))
+        # 1. Получаем данные по лекциям и группам, участвующим в семестре и году (предположим, семестр влияет на название курса)
+        start, end = datetime.date(year,6,1), datetime.date(year,12,30)
+        if semester == 2:
+            start, end = datetime.date(year,1,1), datetime.date(year,6,30)
 
-        results = []
-        for row in cursor.fetchall():
-            course_name, lecture_name, total_students, tech_equipment, year, semester = row
+        cur.execute("""
+            SELECT 
+                lc.name AS course_name,
+                l.id AS lecture_id,
+                l.topic,
+                l.tech_requirements,
+                COUNT(DISTINCT s.id) AS total_listeners
+            FROM lecture_course lc
+            JOIN lectures l ON l.course_id = lc.id
+            JOIN schedule sch ON sch.lecture_id = l.id
+            JOIN attendance AS A ON A.schedule_id = A.id
+            JOIN groups g ON sch.group_id = g.id
+            JOIN students s ON s.group_id = g.id
+            WHERE A.attendance_date BETWEEN %s and %s 
+            GROUP BY lc.name, l.id, l.topic, l.tech_requirements
+        """, (start, end))
 
-            # Рассчитываем требуемую вместимость (студенты + 20% запаса)
-            required_capacity = int(total_students * 1.2)
+        lectures = cur.fetchall()
+        logging.info("Auditorium requirements fetched ", len(lectures))
 
-            # Определяем технические требования
-            tech_requirements = []
-            if tech_equipment:
-                tech_requirements.extend(["projector", "sound_system"])
-            if "программирование" in course_name.lower():
-                tech_requirements.append("computers")
-            if "лабораторная" in lecture_name.lower():
-                tech_requirements.append("lab_equipment")
+        for course_name, lecture_id, topic, tech_requirements, total_listeners in lectures:
+            # 2. Используем Neo4j для уточнения, если это специальная лекция или требуется особое оборудование
+            with neo4j_driver.session() as session:
+                special_flag = session.run(
+                    """
+                    MATCH (lec:Lecture {id: $lecture_id})
+                    RETURN lec.is_special AS is_special
+                    """,
+                    lecture_id=lecture_id
+                ).single()
 
-            results.append(ClassroomRequirement(
-                course_name=course_name,
-                lecture_name=lecture_name,
-                required_capacity=required_capacity,
-                tech_requirements=tech_requirements,
-                total_students=total_students,
-                semester=semester,
-                year=year
-            ))
+                is_special = special_flag["is_special"] if special_flag else False
+            result.append({
+                "course_name":course_name,
+                "topic":topic + (" (Спец.)" if is_special else ""),
+                "tech_requirements":tech_requirements or "Не указаны",
+                "total_listeners":total_listeners
+            })
 
-        return results
+        cur.close()
+        return result
 
     except Exception as e:
-        logger.error(f"Database error: {str(e)}")
-        raise HTTPException(status_code=500, detail="Database error")
-    finally:
-        cursor.close()
-        conn.close()
+        return [{"error": str(e)}]
